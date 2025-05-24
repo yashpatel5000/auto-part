2d74a1baaae38916b25ae689cfe63080d98b56e2
Jigar Khalas <khalasjigar0861@gmail.com>
2025-05-24 16:51:25 +0530

    Added sandbox in puppeter.

--- utils/download-image.js
+++ utils/download-image.js
@@ -4,8 +4,12 @@ import { SIGNED_URL_CONTENT_TYPE } from "./constant.js";
 
 const downloadImage = async (url) => {
   const fileName = `${Date.now()}.jpeg`;
+  
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
-  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.setUserAgent(
@@ -17,20 +21,27 @@ const downloadImage = async (url) => {
   const response = await page.goto(url, { waitUntil: "networkidle2" });
 
   if (!response || response.status() !== 200) {
-    console.error("❌ Failed to load image:", response ? response.status() : "no response");
+    console.error(
+      "❌ Failed to load image:",
+      response ? response.status() : "no response"
+    );
     await browser.close();
     return null;
   }
 
   const buffer = await response.buffer();
 
-  await uploadBufferToS3(buffer,fileName,SIGNED_URL_CONTENT_TYPE[url.split('.').pop()]);
+  await uploadBufferToS3(
+    buffer,
+    fileName,
+    SIGNED_URL_CONTENT_TYPE[url.split(".").pop()]
+  );
 
   await browser.close();
- 
+
   return {
     filePath: `https://auto-part.s3.eu-north-1.amazonaws.com/${fileName}`,
-    fileName
+    fileName,
   };
 };
 
